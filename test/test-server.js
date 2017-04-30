eba7ffd59e2f256065f57a47c7c9b111e674a139
Benjamin E White <benjamin.e.white1@gmail.com>
2017-04-29 21:25:21 -0400

    Return promise, don't use `done` callback
    Closes #3

--- test/test-server.js
+++ test/test-server.js
@@ -18,8 +18,8 @@ describe('Blog Posts', function() {
     return closeServer();
   });
 
-  it('should list items on GET', function(done) {
-    chai.request(app)
+  it('should list items on GET', function() {
+    return chai.request(app)
       .get('/blog-posts')
       .end(function(err, res) {
         res.should.have.status(200);
@@ -32,10 +32,9 @@ describe('Blog Posts', function() {
             'id', 'title', 'content', 'author', 'publishDate')
         });
       });
-    done();
   });
 
-  it('should add a blog post on POST', function(done) {
+  it('should add a blog post on POST', function() {
     const newPost = {
       title: 'Lorem ip some',
       content: 'foo foo foo foo',
@@ -43,7 +42,7 @@ describe('Blog Posts', function() {
     };
     const expectedKeys = ['id', 'publishDate'].concat(Object.keys(newPost));
 
-    chai.request(app)
+    return chai.request(app)
       .post('/blog-posts')
       .send(newPost)
       .end(function(err, res) {
@@ -55,23 +54,21 @@ describe('Blog Posts', function() {
         res.body.content.should.equal(newPost.content);
         res.body.author.should.equal(newPost.author)
       });
-      done();
   });
 
-  it('should error if POST missing expected values', function(done) {
+  it('should error if POST missing expected values', function() {
     const badRequestData = {};
-    chai.request(app)
+    return chai.request(app)
       .post('/blog-posts')
       .send(badRequestData)
       .end(function(err, res) {
         res.should.have.status(400);
-      })
-    done();
+      });
   });
 
-  it('should update blog posts on PUT', function(done) {
+  it('should update blog posts on PUT', function() {
 
-    chai.request(app)
+    return chai.request(app)
       // first have to get
       .get('/blog-posts')
       .end(function(err, res) {
@@ -79,29 +76,27 @@ describe('Blog Posts', function() {
           title: 'connect the dots',
           content: 'la la la la la'
         });
-        chai.request(app)
+        return chai.request(app)
           .put(`/blog-posts/${res.body[0].id}`)
           .send(updatedPost)
           .end(function(err, res) {
             res.should.have.status(204);
             res.should.be.json;
           });
-      })
-      done();
+      });
   });
 
-  it('should delete posts on DELETE', function(done) {
-    chai.request(app)
+  it('should delete posts on DELETE', function() {
+    return chai.request(app)
       // first have to get
       .get('/blog-posts')
       .end(function(err, res) {
-        chai.request(app)
+        return chai.request(app)
           .delete(`/blog-posts/${res.body[0].id}`)
           .end(function(err, res) {
             res.should.have.status(204);
           });
-      })
-      done();
+      });
   });
 
 });
