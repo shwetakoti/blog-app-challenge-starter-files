34498f7ad8c3703404b7801588c5e356974a7e33
“Shweta <choti@ravikirans-air.lan>
2018-04-04 07:16:26 -0700

    blogapp with mongoose

--- server.js
+++ server.js
@@ -1,14 +1,92 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const{PORT,DATABASE_URL} = require('./config');
+const{blogApp} = require('./models');
 
 const app = express();
+app.use(bodyParser.json()) ;
 
-
-app.use(morgan('common'));
-
-// you need to import `blogPostsRouter` router and route
-// requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
-
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+app.get('/posts',(req,res)=>{
+  blogApp.find().then(posts=>{
+    res.json(posts.map(post=>post.serialize()));
+  }).catch(err=>{
+    console.error(err);
+    res.status(500).json({"Internal Server Error"});
+  });
 });
+
+//get by ID
+app.get('/posts/:id',(req,res)=>{
+  blogApp.findById(req.params.id).then(post=>post.serialize()).catch(err=>{
+    console.error(err);
+    res.status(500).json({"Internal Server Error"});
+  });
+});
+
+//post method
+
+app.post('/posts',(req,res)=>{
+  const reqFields = ['title','content','author'];
+  for(let i=0;i<reqFields.length;i++){
+    const field = reqFields[i];
+    if(!(field in req.body)){
+      const message = `required field ${field} is missing`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+})
+
+
+// closeServer needs access to a server object, but that only
+// gets created when `runServer` runs, so we declare `server` here
+// and then assign a value to it in run
+let server;
+
+// this function connects to our database, then starts the server
+function runServer(databaseUrl, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
