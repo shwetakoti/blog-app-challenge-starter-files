b71e447e958c9492a61858d7fd326d1d5ab638eb
“Shweta <choti@ravikirans-air.lan>
2018-04-04 18:19:43 -0700

    modified files

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blogapp';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blogapp';
 exports.PORT = process.env.PORT || 8080;
